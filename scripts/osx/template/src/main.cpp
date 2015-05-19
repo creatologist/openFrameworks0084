ac91bc7bb158e4da603bb62893f323289c4a43fe
Christopher Miles <hi.christophermiles@gmail.com>
2015-05-18 23:56:52 -0400

    update

--- scripts/osx/template/src/main.cpp
+++ scripts/osx/template/src/main.cpp
@@ -8,6 +8,19 @@ int main( ){
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
+    /*ofiOSWindowSettings settings;
+    settings.enableRetina = false; // enables retina resolution if the device supports it.
+    settings.enableDepth = false; // enables depth buffer for 3d drawing.
+    settings.enableAntiAliasing = false; // enables anti-aliasing which smooths out graphics on the screen.
+    settings.numOfAntiAliasingSamples = 0; // number of samples used for anti-aliasing.
+    settings.enableHardwareOrientation = false; // enables native view orientation.
+    settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
+    settings.glesVersion = OFXIOS_RENDERER_ES1; // type of renderer to use, ES1, ES2, ES3
+    settings.windowMode = OF_FULLSCREEN;
+    ofCreateWindow(settings);
+    
+    return ofRunApp(new ofApp);*/
+    
 	ofRunApp(new ofApp());
 
 }
