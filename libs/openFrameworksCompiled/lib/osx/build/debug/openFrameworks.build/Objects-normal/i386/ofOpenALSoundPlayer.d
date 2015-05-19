37bc7697791371308d110521d46d203c21a8eb9c
Christopher Miles <hi.christophermiles@gmail.com>
2015-05-19 10:37:59 -0400

    removed openFrameworksCompiled

--- libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofOpenALSoundPlayer.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Stardust/Documents/GitHub/openFrameworks0084/libs/openFrameworks/sound/ofOpenALSoundPlayer.cpp \
-  ../../../../libs/openFrameworks/sound/ofOpenALSoundPlayer.h \
-  /Users/Stardust/Documents/GitHub/openFrameworks0084/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
-  ../../../../libs/glew/include/GL/glew.h \
-  ../../../../libs/tess2/include/tesselator.h
--- libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofQTKitMovieRenderer.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Stardust/Documents/GitHub/openFrameworks0084/libs/openFrameworks/video/ofQTKitMovieRenderer.m \
-  ../../../../libs/openFrameworks/video/ofQTKitMovieRenderer.h
