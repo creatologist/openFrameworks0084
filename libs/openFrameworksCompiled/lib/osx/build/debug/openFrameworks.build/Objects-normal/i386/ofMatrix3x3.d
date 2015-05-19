60a5602c621a8b09381c3201881b9e08a8c1a6ff
Christopher Miles <hi.christophermiles@gmail.com>
2015-05-19 10:43:43 -0400

    update

--- libs/openFrameworksCompiled/lib/osx/build/debug/openFrameworks.build/Objects-normal/i386/ofMatrix3x3.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Stardust/Documents/GitHub/openFrameworks0084/libs/openFrameworks/math/ofMatrix3x3.cpp \
-  ../../../../libs/openFrameworks/math/ofMatrix3x3.h \
-  /Users/Stardust/Documents/GitHub/openFrameworks0084/libs/openFrameworksCompiled/project/osx/../../../openFrameworks/utils/ofConstants.h \
-  ../../../../libs/glew/include/GL/glew.h \
-  ../../../../libs/tess2/include/tesselator.h
